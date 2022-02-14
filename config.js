8af4c24be9f4efc1cb02106b390c390243c00dcb
Matheus Berkenbrock Nedel <matheusbnedel@gmail.com>
2022-02-14 16:51:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   databaseUrl: 'mongodb+srv://admin:salvedaniel@cluster0.kfu7a.mongodb.net/medivia?retryWrites=true&w=majority',
   port: 4000,
   saltFactor: 5,
-  jwtSecret: "paunocudoslaydgartbakjsdhkajshdkjash"
+  jwtSecret: "sshhhhdgartbakjsdhkajshdkjash"
 }
